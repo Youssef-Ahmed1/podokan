78f8da0b047feecb2a474a69f3d393341f2ac4b5
youssefAhmed <moropass1212@gmail.com>
2024-10-10 20:55:45 +0300

    save

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const server = app.listen(process.env.PORT, () => {
 process.on("unhandledRejection", (err) => {
   console.log(`Shutting down the server for ${err.message}`);
   console.log(`shutting down the server for unhandle promise rejection`);
-
+// console.log()
   server.close(() => {
     process.exit(1);
   });
