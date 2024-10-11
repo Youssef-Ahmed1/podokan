228f0e09b7055392ab7bc840c0df9c93d51ffb9f
youssefAhmed <moropass1212@gmail.com>
2024-10-11 17:53:17 +0300

    save

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -7,7 +7,7 @@ const connectDatabase = () => {
       useUnifiedTopology: true,
     })
     .then((data) => {
-      console.log(`mongod connected with server: ${data.connection.host}`);
+      console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
     .catch((error) => {
       console.error("Database connection error:", error);
--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,41 @@
 const app = require("./app");
 const connectDatabase = require("./db/Database");
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
-  console.log(`shutting down the server for handling uncaught exception`);
+  console.log(`Shutting down the server due to uncaught exception`);
+  process.exit(1);
 });
 
-// config
+// Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
     path: "config/.env",
   });
 }
 
-// connect db
+// Connect to database
 connectDatabase();
 
+// Cloudinary Config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
-})
-
-
-// create server
-const server = app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
-  console.log(
-    `Server is running on http://0.0.0.0:${process.env.PORT || 8000}`
-  );
 });
 
-// unhandled promise rejection
+// Create server
+const server = app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${process.env.PORT || 8000}`);
+});
+
+// Unhandled promise rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Shutting down the server for ${err.message}`);
-  console.log(`shutting down the server for unhandle promise rejection`);
-// console.log()
+  console.log(`Shutting down the server due to ${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise rejection`);
+
   server.close(() => {
     process.exit(1);
   });
