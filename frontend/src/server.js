ee4259abca234fef985a4f40d45f4ea5f4619bb2
youssefAhmed <moropass1212@gmail.com>
2024-10-11 20:52:49 +0300

    save

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend_url = "http://localhost:8000";
+export const server = "http://93.127.203.101:8000/api/v2";
+export const backend_url = "http://93.127.203.101:8000";
