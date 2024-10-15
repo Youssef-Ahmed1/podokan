79b3022422d2541c97725ebc1925c0d8bbbc41d9
youssefAhmed <moropass1212@gmail.com>
2024-10-15 17:35:06 +0300

    save

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const connectDatabase = require("./db/Database");
 const cloudinary = require("cloudinary").v2;
 const https = require('https');
 const fs = require('fs');
+
 // Handling uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
@@ -27,20 +28,23 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+let server;
+
 if (process.env.NODE_ENV === "PRODUCTION") {
   const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/testpodokan.store/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/testpodokan.store/fullchain.pem')
   };
-  const server = https.createServer(options, app);
-  server.listen(8000, '127.0.0.1', () => {
-    console.log(`HTTPS Server is running on port 8000`);
+  server = https.createServer(options, app);
+  server.listen(process.env.PORT, '127.0.0.1', () => {
+    console.log(`HTTPS Server is running on port ${process.env.PORT}`);
   });
 } else {
-  const server = app.listen(8000, '127.0.0.1', () => {
-    console.log(`HTTP Server is running on port 8000`);
+  server = app.listen(process.env.PORT, '127.0.0.1', () => {
+    console.log(`HTTP Server is running on port ${process.env.PORT}`);
   });
 }
+
 // Unhandled promise rejection
 process.on("unhandledRejection", (err) => {
   console.log(`Shutting down the server due to ${err.message}`);
