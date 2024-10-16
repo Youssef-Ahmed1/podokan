0ea30b1e4cfa17783b4e784423bae43939fc48d6
youssefAhmed <moropass1212@gmail.com>
2024-10-16 18:59:59 +0300

    save

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from "../components/Layout/Header";
 import Hero from "../components/Route/Hero/Hero";
-import Categories from "../components/Route/Categories/Categories";
 import BestDeals from "../components/Route/BestDeals/BestDeals";
 import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 // import Events from "../components/Events/Events";
@@ -15,7 +14,6 @@ const HomePage = () => {
     <div>
         <Header activeHeading={1} />
         <div className = "bg-[#151523] rounded-b-lg">
-        <Categories />
           <ArtistSign/>
         </div>
         <Hero />
