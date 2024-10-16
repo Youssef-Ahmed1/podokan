7349b2a171f0c4ecd3ff0abf1290b1f8cc04cca5
youssefAhmed <moropass1212@gmail.com>
2024-10-16 19:11:31 +0300

    save

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -5,14 +5,21 @@ import { categoriesData } from '../../static/data';
 const Navbar = ({ active }) => {
   return (
     <div className="flex items-center justify-center 800px:gap-8">
-      <Link to="/all-designs" className="text-white hover:text-[#f6b92e]">
+      <Link 
+        to="/all-designs" 
+        className={`text-white hover:text-[#f6b92e] transition-colors duration-300 ${
+          active === "All" ? "text-[#f6b92e]" : ""
+        }`}
+      >
         Shop All Designs
       </Link>
       {categoriesData.map((category) => (
         <Link 
           key={category.id} 
           to={`/category/${category.title.toLowerCase()}`} 
-          className={`flex items-center text-white hover:text-[#f6b92e] ${active === category.title ? "text-[#f6b92e]" : ""}`}
+          className={`flex items-center text-white hover:text-[#f6b92e] transition-colors duration-300 ${
+            active === category.title ? "text-[#f6b92e]" : ""
+          }`}
         >
           <img src={category.icon} alt={category.title} className="w-5 h-5 mr-1" />
           {category.title}
