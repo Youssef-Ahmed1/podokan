59b20e025c0c3e42c10c3bcbaed520ee16b9d1b0
youssefAhmed <moropass1212@gmail.com>
2024-10-28 20:29:35 +0300

    product details modify

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,41 +1,22 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async (options) => {
-  try {
-    // Create transporter
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-      port: 465,
-      secure: true, // use SSL
-      auth: {
-        user: 'moropass1212@gmail.com',
-        pass: 'zqkxjfttywkzisss'
-      },
-      tls: {
-        rejectUnauthorized: false // Only if you're having certificate issues
-      }
-    });
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'moropass1212@gmail.com',
+      pass: 'zqkxjfttywkzisss'
+    }
+  });
 
-    // Email content
-    const mailOptions = {
-      from: '"PODokan Support" <moropass1212@gmail.com>',
-      to: options.email,
-      subject: options.subject,
-      html: options.html
-    };
+  const mailOptions = {
+    from: 'moropass1212@gmail.com',
+    to: options.email,
+    subject: options.subject,
+    html: options.html
+  };
 
-    // Send email
-    const info = await transporter.sendMail(mailOptions);
-    return info;
-  } catch (error) {
-    console.error("Email Error:", {
-      code: error.code,
-      message: error.message,
-      stack: error.stack,
-      command: error.command
-    });
-    throw error;
-  }
+  return await transporter.sendMail(mailOptions);
 };
 
 module.exports = sendMail;
