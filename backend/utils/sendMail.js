2af231501fb26efd182d7aa6f66e123acba13e56
youssefAhmed <moropass1212@gmail.com>
2024-10-28 19:25:17 +0300

    product details modify

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -6,14 +6,14 @@ const sendMail = async (options) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: true, // true for port 465
+      secure: true, 
       service: process.env.SMTP_SERVICE,
       auth: {
         user: process.env.SMTP_MAIL,
         pass: process.env.SMTP_PASSWORD,
       },
       tls: {
-        rejectUnauthorized: false // Only use this in development
+        rejectUnauthorized: false 
       }
     });
 
