c448280748e306dd31e780b625a04c8781a0d8eb
youssefAhmed <moropass1212@gmail.com>
2024-11-01 19:54:17 +0200

    product details modify

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -1,35 +1,19 @@
+const sendShopToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
 
-const sendShopToken = (shop, statusCode, res) => {
-  const token = shop.getJwtToken();
-  
-  // Options for cookies
-  const cookieOptions = {
-      expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-      path: '/',
-      domain: process.env.NODE_ENV === 'PRODUCTION' ? '.testpodokan.store' : undefined
+  // Options for cookie
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
   };
 
-  // Set seller authorization header
-  const authHeader = `Bearer ${token}`;
-  
-  // Remove password from response
-  if (shop.toJSON) {
-      shop = shop.toJSON();
-  }
-  if (shop.password) {
-      delete shop.password;
-  }
-
-  return res
-      .status(statusCode)
-      .cookie("seller_token", token, cookieOptions)
-      .set('Seller-Authorization', authHeader)
-      .json({
-          success: true,
-          shop,
-          token,
-      });
+  res.status(statusCode).cookie("seller_token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
 };
+
+module.exports = sendShopToken;
