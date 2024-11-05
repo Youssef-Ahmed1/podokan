f5052a729c5de140b7e3438c613bf0e77b65c893
youssefAhmed <moropass1212@gmail.com>
2024-11-05 02:12:56 +0200

    product details modify

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,23 +4,21 @@ module.exports = {
     script: "server.js",
     instances: 1,
     exec_mode: "cluster",
-    watch: ["./"], // Enable watch mode
+    watch: ["./"],
     ignore_watch: [
       "node_modules",
       "uploads",
       "logs",
-      ".git",
-      "*.log"
+      ".git"
     ],
-    max_memory_restart: "1G",
     env_production: {
-      NODE_ENV: "PRODUCTION"
+      NODE_ENV: "PRODUCTION",
+      PORT: 8000
     },
+    max_memory_restart: "1G",
     error_file: "logs/error.log",
     out_file: "logs/out.log",
     time: true,
-    max_restarts: 10,
-    restart_delay: 4000,
     listen_timeout: 50000,
     kill_timeout: 5000
   }]
