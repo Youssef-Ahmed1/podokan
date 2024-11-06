62f5ac87d945a5b0948793359492a6539df50b73
youssefAhmed <moropass1212@gmail.com>
2024-11-07 00:25:33 +0200

    save

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -4,16 +4,28 @@ module.exports = {
     script: "server.js",
     instances: 1,
     exec_mode: "cluster",
+    watch: ["./"],
+    ignore_watch: [
+      "node_modules",
+      "uploads",
+      "logs",
+      ".git"
+    ],
     env_production: {
       NODE_ENV: "PRODUCTION",
-      PORT: 8000,
-      DB_URL: "mongodb+srv://youssefahmed:zxczxcZXC123@cluster0.iv8sqpb.mongodb.net/podShop"
+      PORT: 8000
     },
-    watch: true,
-    max_memory_restart: '1G',
+    max_memory_restart: "1G",
     error_file: "logs/error.log",
     out_file: "logs/out.log",
     time: true,
-    log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
+    combine_logs: true,
+    merge_logs: true,
+    max_restarts: 10,
+    restart_delay: 4000,
+    listen_timeout: 50000,
+    kill_timeout: 10000,
+    max_memory_restart: '1G'
   }]
 };
