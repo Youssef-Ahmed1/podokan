7c45cb9bbf4b7c2079a6c19409c8ca7fb57c1cd0
youssefAhmed <moropass1212@gmail.com>
2024-11-07 00:32:04 +0200

    save

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,14 +3,8 @@ module.exports = {
     name: "podokan",
     script: "server.js",
     instances: 1,
-    exec_mode: "cluster",
-    watch: ["./"],
-    ignore_watch: [
-      "node_modules",
-      "uploads",
-      "logs",
-      ".git"
-    ],
+    exec_mode: "fork",
+    watch: false,
     env_production: {
       NODE_ENV: "PRODUCTION",
       PORT: 8000
@@ -19,13 +13,6 @@ module.exports = {
     error_file: "logs/error.log",
     out_file: "logs/out.log",
     time: true,
-    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-    combine_logs: true,
-    merge_logs: true,
-    max_restarts: 10,
-    restart_delay: 4000,
-    listen_timeout: 50000,
-    kill_timeout: 10000,
-    max_memory_restart: '1G'
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z"
   }]
 };
