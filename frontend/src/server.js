01a915d3b64591a04deef0d0404c453eb8c0c64b
youssefAhmed <moropass1212@gmail.com>
2024-11-07 15:49:51 +0200

    save

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,20 +1,3 @@
 export const server = "https://testpodokan.store/api/v2";
 export const backend_url = "https://testpodokan.store";
 
-
-axios.defaults.withCredentials = true;
-axios.defaults.headers.common['Accept'] = 'application/json';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.interceptors.response.use(
-    (response) => response,
-    (error) => {
-      if (error.response?.status === 401) {
-        // Clear tokens on unauthorized
-        localStorage.removeItem('token');
-        localStorage.removeItem('seller_token');
-        delete axios.defaults.headers.common['Authorization'];
-        delete axios.defaults.headers.common['Seller-Authorization'];
-      }
-      return Promise.reject(error);
-    }
-  );
