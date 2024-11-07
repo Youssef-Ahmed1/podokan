835f83d5b50428e40ae30eca8166000477e852f5
youssefAhmed <moropass1212@gmail.com>
2024-11-07 02:54:43 +0200

    product details modify

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -5,13 +5,18 @@ module.exports = {
     instances: 1,
     exec_mode: "fork",
     watch: false,
-    max_memory_restart: "500M",
+    max_memory_restart: "1G",
     env: {
       NODE_ENV: "production",
       PORT: 8000
     },
     env_production: {
       NODE_ENV: "production"
-    }
+    },
+    node_args: ['--max-old-space-size=2048'],
+    kill_timeout: 3000,
+    wait_ready: true,
+    listen_timeout: 30000,
+    time: true
   }]
 };
