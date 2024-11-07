5402f6df35d176de35132a983facf3eeae5608d6
youssefAhmed <moropass1212@gmail.com>
2024-11-07 22:45:37 +0200

    save

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,26 +1,20 @@
 module.exports = {
   apps: [{
     name: "podokan",
-    script: "server.js",
-    instances: "max",
-    exec_mode: "cluster",
+    script: "./server.js",
     watch: false,
-    max_memory_restart: "1G",
-    env_production: {
-      NODE_ENV: "PRODUCTION",
-      PORT: 8000,
-      DB_URL: "mongodb+srv://youssefahmed:zxczxcZXC123@cluster0.iv8sqpb.mongodb.net/podShop",
-      JWT_SECRET_KEY: "a3ea75cf2028f52e821215ff6b02b24029b48554f0de85da847bdb755bd88641",
-      JWT_EXPIRES: "7d",
-      ACTIVATION_SECRET: "PWj0fI#&DsZY9w$8tHe11*yr9F45K*j2xj&fceGZ!tEnMNZcEN",
-      SMTP_HOST: "smtp.gmail.com",
-      SMTP_PORT: 465,
-      SMTP_SERVICE: "gmail",
-      SMTP_MAIL: "moropass1212@gmail.com",
-      SMTP_PASSWORD: "zqkxjfttywkzisss",
-      CLOUDINARY_NAME: "dkot9tyjm",
-      CLOUDINARY_API_KEY: "411713446345318",
-      CLOUDINARY_API_SECRET: "s01XENodwewxd9U4OuuzUB5-ij8"
-    }
+    env: {
+      NODE_ENV: "production",
+      PORT: 8000
+    },
+    error_file: "./logs/err.log",
+    out_file: "./logs/out.log",
+    log_file: "./logs/combined.log",
+    time: true,
+    instance_var: 'INSTANCE_ID',
+    max_memory_restart: '1G',
+    restart_delay: 4000,
+    max_restarts: 10,
+    autorestart: true
   }]
 };
