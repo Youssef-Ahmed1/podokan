3b8cacd82b1405236734cf28569ef7e0eaad320e
youssefAhmed <moropass1212@gmail.com>
2024-11-07 02:24:56 +0200

    product details modify

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,4 +1,3 @@
-// ecosystem.config.js
 module.exports = {
   apps: [{
     name: "podokan",
@@ -6,18 +5,13 @@ module.exports = {
     instances: 1,
     exec_mode: "fork",
     watch: false,
-    max_memory_restart: "1G",
+    max_memory_restart: "500M",
     env: {
       NODE_ENV: "production",
       PORT: 8000
     },
-    error_file: "logs/error.log",
-    out_file: "logs/out.log",
-    time: true,
-    merge_logs: true,
-    log_date_format: "YYYY-MM-DD HH:mm:ss Z",
-    max_restarts: 5,
-    restart_delay: 5000,
-    autorestart: true
+    env_production: {
+      NODE_ENV: "production"
+    }
   }]
 };
