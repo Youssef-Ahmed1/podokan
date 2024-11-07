2eaeea154a9c24358288ed43dc88837a78658e31
youssefAhmed <moropass1212@gmail.com>
2024-11-07 23:44:41 +0200

    save

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,7 +2,10 @@ module.exports = {
   apps: [{
     name: "podokan",
     script: "./server.js",
+    instances: 1,
+    exec_mode: "fork",
     watch: false,
+    max_memory_restart: "300M",
     env: {
       NODE_ENV: "production",
       PORT: 8000
@@ -11,10 +14,7 @@ module.exports = {
     out_file: "./logs/out.log",
     log_file: "./logs/combined.log",
     time: true,
-    instance_var: 'INSTANCE_ID',
-    max_memory_restart: '1G',
-    restart_delay: 4000,
     max_restarts: 10,
-    autorestart: true
+    restart_delay: 4000
   }]
 };
