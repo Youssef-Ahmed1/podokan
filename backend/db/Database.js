0870e2faf0f280ce22a19933dd683223c79f3148
youssefAhmed <moropass1212@gmail.com>
2024-11-07 17:11:28 +0200

    save

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,26 +1,13 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  const dbUrl = process.env.DB_URL;
-  
-  if (!dbUrl) {
-    console.error('DB_URL is not defined in environment variables');
-    process.exit(1);
-  }
-
-  console.log('Attempting to connect to MongoDB...');
-  
   mongoose
-    .connect(dbUrl, {
+    .connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then((data) => {
-      console.log(`MongoDB connected with server: ${data.connection.host}`);
-    })
-    .catch((error) => {
-      console.error("Database connection error:", error);
-      process.exit(1);
+      console.log(`mongod connected with server: ${data.connection.host}`);
     });
 };
 
