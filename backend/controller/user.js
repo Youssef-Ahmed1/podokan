87e055b33522aefb469b84522efa363a3c7877a3
youssefAhmed <moropass1212@gmail.com>
2024-11-10 20:44:09 +0200

    save

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -111,7 +111,26 @@ const createActivationToken = (user) => {
     expiresIn: "5m",
   });
 };
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
 
+  const cookieOptions = {
+    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'PRODUCTION',
+    sameSite: 'strict',
+    domain: process.env.NODE_ENV === 'PRODUCTION' ? '.testpodokan.store' : undefined,
+    path: '/'
+  };
+
+  res.status(statusCode)
+    .cookie('token', token, cookieOptions)
+    .json({
+      success: true,
+      token,
+      user
+    });
+};
 // activate user
 router.post(
   "/activation",
