2bd95d31f3d4948aa692a065177699376e1e4a9b
youssefAhmed <moropass1212@gmail.com>
2024-11-10 21:33:31 +0200

    save

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,27 +1,22 @@
-const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const sendToken = (user, statusCode, res) => {
-  const token = user.getJwtToken();
-
-  const cookieOptions = {
-    expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'PRODUCTION',
-    sameSite: 'strict',
-    path: '/',
-    domain: process.env.NODE_ENV === 'PRODUCTION' ? '.testpodokan.store' : undefined
-  };
-
-  // Remove password from output
-  const userData = user.toObject();
-  delete userData.password;
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: "7d",
+  });
 
   res.status(statusCode)
-    .cookie('token', token, cookieOptions)
+    .cookie("token", token, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      domain: ".testpodokan.store"
+    })
     .json({
       success: true,
+      user,
       token,
-      user: userData
     });
 };
 
