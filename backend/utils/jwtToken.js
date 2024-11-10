259945aabacdf347903b9417b47accc97bcace30
youssefAhmed <moropass1212@gmail.com>
2024-11-10 21:07:06 +0200

    save

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,8 +1,7 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJwtToken();
 
-  // Cookie options
-  const options = {
+  const cookieOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'PRODUCTION',
@@ -11,12 +10,16 @@ const sendToken = (user, statusCode, res) => {
     domain: process.env.NODE_ENV === 'PRODUCTION' ? '.testpodokan.store' : undefined
   };
 
+  // Remove password from output
+  const userData = user.toObject();
+  delete userData.password;
+
   res.status(statusCode)
-    .cookie("token", token, options)
+    .cookie('token', token, cookieOptions)
     .json({
       success: true,
-      user,
       token,
+      user: userData
     });
 };
 
