5af5572878987adbbf46592a55c97ae3d0b25140
youssefAhmed <moropass1212@gmail.com>
2024-12-06 21:48:05 +0200

    save

--- frontend/src/components/Layout/AdminHeader.jsx
+++ frontend/src/components/Layout/AdminHeader.jsx
@@ -11,10 +11,11 @@ const AdminHeader = () => {
     const {user} = useSelector((state) => state.user);
 
   return (
-         <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
+         <div className="w-full h-[80px] bg-indigo-500 shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
         <Link to="/">
           <img
+          size={20}
             src={`${siteLogo}`}
             alt=""
           />
--- frontend/src/components/Shop/Layout/DashboardHeader.jsx
+++ frontend/src/components/Shop/Layout/DashboardHeader.jsx
@@ -1,32 +1,18 @@
 import React from "react";
 import { FiPackage, FiShoppingBag, FiLogOut } from "react-icons/fi";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import siteLogo from "../../../Assests/siteLogo.png";
-import { logout } from "../../../redux/actions/user";
-import { toast } from "react-toastify";
 
 const DashboardHeader = () => {
   const { seller } = useSelector((state) => state.seller);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    try {
-      await dispatch(logout());
-      navigate("/");
-      toast.success("Logout successful!");
-    } catch (error) {
-      toast.error("Logout failed");
-    }
-  };
 
   return (
     <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
         <Link to="/dashboard">
-          <img src={siteLogo} alt="Site Logo" className="h-8" />
+          <img src={siteLogo} alt="" className="h-16" />
         </Link>
       </div>
       <div className="flex items-center">
@@ -48,25 +34,20 @@ const DashboardHeader = () => {
               className="mx-5 cursor-pointer"
             />
           </Link>
-          {seller?._id && (
+          {seller?.avatar?.url && (
             <Link to={`/shop/${seller._id}`}>
               <img
-                src={seller.avatar?.url}
-                alt="Shop Avatar"
+                src={seller.avatar.url}
+                alt=""
                 className="w-[50px] h-[50px] rounded-full object-cover"
               />
             </Link>
           )}
-          <button
-            onClick={handleLogout}
-            className="ml-4 flex items-center text-gray-600 hover:text-red-500"
-          >
-            <FiLogOut size={24} />
-          </button>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default DashboardHeader;
