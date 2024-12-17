d1025cfcae97588bb4c0091dbad4d62dfdae6584
youssefAhmed <moropass1212@gmail.com>
2024-12-17 20:10:26 +0200

    save

--- frontend/src/components/Route/BestDeals/BestDeals.jsx
+++ frontend/src/components/Route/BestDeals/BestDeals.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState   } from "react";
 import { useSelector } from "react-redux";
 import styles from "../../../styles/styles";
 import ProductCard from "../ProductCard/ProductCard";
