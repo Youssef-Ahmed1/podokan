97d7a8c9f066100cb83869893778941714b800ca
youssefAhmed <moropass1212@gmail.com>
2024-12-18 00:45:54 +0200

    save

---
+++ frontend/src/routes/CategoryFinder/CategoryFinder.jsx
@@ -0,0 +1,54 @@
+
+  const categories = [
+    { 
+      id: 'funny',
+      name: 'Comedian',
+      icon: '😄',
+      description: 'For the ones who love to make others laugh'
+    },
+    { 
+      id: 'music',
+      name: 'Musical Soul',
+      icon: '🎵',
+      description: 'For music lovers and rhythm makers'
+    },
+    { 
+      id: 'movies',
+      name: 'Movie Buff',
+      icon: '🎬',
+      description: 'For cinema enthusiasts'
+    },
+    { 
+      id: 'tv',
+      name: 'Channel Surfer',
+      icon: '📺',
+      description: 'For TV show binge-watchers'
+    },
+    { 
+      id: 'sports',
+      name: 'Sports Fanatic',
+      icon: '⚽',
+      description: 'For the game lovers'
+    },
+    { 
+      id: 'anime',
+      name: 'Anime Lover',
+      icon: '🎌',
+      description: 'For manga and anime fans'
+    },
+    { 
+      id: 'scifi',
+      name: 'Sci-Fi Nerd',
+      icon: '👽',
+      description: 'For future tech enthusiasts'
+    },
+    { 
+      id: 'vintage',
+      name: 'Vintage',
+      icon: '📻',
+      description: 'For retro style lovers'
+    }
+  ];
+
+
+
