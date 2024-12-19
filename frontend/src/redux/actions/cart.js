bd5ecf4d88f7d6d1b67e25d26f2bf31763396270
youssefAhmed <moropass1212@gmail.com>
2024-12-19 21:35:56 +0200

    save

--- frontend/src/redux/actions/cart.js
+++ frontend/src/redux/actions/cart.js
@@ -41,3 +41,13 @@ export const addTocart = (data) => async (dispatch, getState) => {
     return { success: false, message: error.message };
   }
 };
+
+// remove from cart
+export const removeFromCart = (data) => async (dispatch, getState) => {
+  dispatch({
+    type: "removeFromCart",
+    payload: data._id,
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cart));
+  return data;
+};
