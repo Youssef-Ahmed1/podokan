252391b6c868e992ca437725efe3c3331b1c9c65
youssefAhmed <moropass1212@gmail.com>
2025-02-06 16:34:09 +0200

    save

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   apps: [{
     name: "podokan",
@@ -6,6 +8,7 @@ module.exports = {
       NODE_ENV: "production",
       PORT: 8000
     },
+    env_file: path.join(__dirname, 'config', '.env'),
     instances: 1,
     exec_mode: "fork",
     watch: false,
@@ -13,6 +16,7 @@ module.exports = {
     error_file: "./logs/err.log",
     out_file: "./logs/out.log",
     log_file: "./logs/combined.log",
-    time: true
+    time: true,
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z"
   }]
 };
